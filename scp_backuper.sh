31dbc208f6069c9d8ea00ecb80f897b76179c21e
sameragi <namet42@yahoo.co.jp>
2016-05-07 02:23:38 +0900

    wrote comment

--- scp_backuper.sh
+++ scp_backuper.sh
@@ -35,41 +35,48 @@ done
 #echo "BACKUP_TARGET = ${BACKUP_TARGET}"
 #echo "DATE_FILE = ${DATE_FILE}"
 
+##第一引数のディレクトリ内の更新されたファイルやディレクトリをコピーする
+##再帰により第一引数のディレクトリ以下の更新されたディレクトリをすべて探索する
 explorer () {
-        TARGET_FILES=""
+        TARGET_FILES="" #更新されてるファイルのパスをスペース区切りで入れる
         fileArray=()
-        dirArray=()
+        dirArray=() #既存のディレクトリを入れる配列
+
         for filePath in ${1}*; do
                 if [ -f $filePath ]; then
                         fileArray+=("$filePath")
                 fi
         done
-        #echo "------File----"
+
         for i in ${fileArray[@]}; do
                 if [ $i -nt ${DATE_FILE} ]; then
                         TARGET_FILES="${TARGET_FILES} $i"
                 fi
         done
+
         #echo "TARGET_FILES : ${TARGET_FILES}"
+
         if [ "$TARGET_FILES" != "" ]; then
                 f=`scp -r -i ${KEY} -P ${PORT} ${TARGET_FILES} ${USER_NAME}@${IP}:${BACKUP_DIR}/$2`
                 echo $f
         fi
-        #echo "------Dir-----"
+
         for i in `ls ${1} -F | grep /`; do
-                flag=0
+                flag=0 # 既存のディレクトリだったら1になる
                 for line in `cat ${KNOWN_DIR_FILE}`; do
                         if [ "$1$i" = $line ]; then
                                 flag=1
                                 dirArray+=("$i")
                         fi
                 done
+
                 if [ $flag = 0 ]; then
                         f=`scp -r -i ${KEY} -P ${PORT} $1$i ${USER_NAME}@${IP}:${BACKUP_DIR}/$2`
                         echo $f
                         echo "$1$i" >>${KNOWN_DIR_FILE}
                 fi
         done
+
         for i in ${dirArray[@]}; do
                 if [ "$1$i" -nt ${DATE_FILE} ]; then
                         explorer "$1$i" "$2$i" "$2"
@@ -77,7 +84,9 @@ explorer () {
         done
 }
 
-firstDirArray=()
+##known_dirを作るためにディレクトリを集める関数。
+##第一引数のディレクトリ内のすべてのディレクトリをfirstDirArrayに入れる。       
+##再帰により第一引数のディレクトリ以下のディレクトリをすべて探してfirstDirArrayに入れる。
 firstExplorer() {
         dirArray=()
         for dirPath in ${1}/*; do
